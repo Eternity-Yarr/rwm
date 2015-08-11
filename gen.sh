fc6af11e0694900f0a4ceedc44f823ae43dec5c1
Dmitry V <savraz@gmail.com>
2015-08-11 23:53:35 +0300

    cargo new

--- gen.sh
+++ gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-bindgen -l libxcb -match ../libxcb/src/xcb.h -o xcb.rs ../libxcb/src/xcb.h
-bindgen -l libxcb -match ../libxcb/src/xcbext.h -o xcbext.rs ../libxcb/src/xcbext.h
+bindgen -l libxcb -match ../libxcb/src/xcb.h -o src/xcb.rs ../libxcb/src/xcb.h
+bindgen -l libxcb -match ../libxcb/src/xcbext.h -o src/xcbext.rs ../libxcb/src/xcbext.h
